ff8f0e1ad806f7d056b015b408068abaa92fcf77
Roman Garnett <romanempire@gmail.com>
2012-03-22 14:23:45 +0100

    initial commit

---
+++ optimal_learning/diagnostics/evaluate_classifier.m
@@ -0,0 +1,23 @@
+function [log_likelihood, accuracy, confusion_matrix] = ...
+      evaluate_classifier(data, responses, train_ind, test_ind, ...
+                          probability_function)
+
+  num_test = numel(test_ind);
+  num_classes = max(responses);
+
+  probabilities = probability_function(data, responses, train_ind, ...
+          test_ind);
+
+  log_likelihood = 0;
+  for i = 1:num_test
+    log_likelihood = log_likelihood + log(probabilities(i, responses(test_ind(i))));
+  end
+
+  [~, predictions] = max(probabilities, [], 2);
+  confusion_matrix = sparse(predictions, responses(test_ind), ...
+                            ones(num_test, 1), num_classes, num_classes);
+  confusion_matrix = full(confusion_matrix);
+
+  accuracy = sum(diag(confusion_matrix)) / num_test;
+
+end
