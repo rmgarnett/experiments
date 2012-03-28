a8af46fc166fdb521e318ffe2549d97dbd9725b8
Roman Garnett <romanempire@gmail.com>
2012-03-27 23:29:36 -0400

    mex implementations

--- optimal_learning/similarity_learning/similarity_functions/wl_subtree_kernel.m
+++ optimal_learning/similarity_learning/similarity_functions/wl_subtree_kernel.m
@@ -44,72 +44,37 @@ function kernel_matrix = wl_subtree_kernel(data, responses, graph_ind, ...
 
   normalize = exist('normalize', 'var') && normalize;
 
-  used_graphs = union(train_graphs, test_graphs);
-
-  used_ind = false(size(data, 1), 1);
-  for i = 1:numel(used_graphs)
-    used_graph_ind = (graph_ind == used_graphs(i));
-
-    used_ind = used_ind | used_graph_ind;
-    graph_ind(used_graph_ind) = i;
-
-    train_graphs(train_graphs == used_graphs(i)) = i;
-    test_graphs(test_graphs   == used_graphs(i)) = i;
-  end
-
-  data      = data(used_ind, used_ind);
-  responses = responses(used_ind);
-  graph_ind(~used_ind) = 0;
-
   num_train_graphs = numel(train_graphs);
   num_test_graphs  = numel(test_graphs);
-  num_graphs       = numel(used_graphs);
 
   if (normalize)
     train_variances = zeros(num_train_graphs, 1);
-    test_variances = zeros(1, num_test_graphs);
+    test_variances  = zeros(1, num_test_graphs);
   end
-  
+
   kernel_matrix = zeros(num_train_graphs, num_test_graphs);
 
   iteration = 0;
   while (true)
-    label_set = 1:max(responses); % unique(responses);
-    num_labels = numel(label_set);
 
-    % the contribution to the graph feature vectors at every step
-    % is simply the counts of each node label on the graph
-    feature_vectors = zeros(num_graphs, num_labels);
-    for i = 1:num_graphs
-      ind = (graph_ind == i);
-      labels = responses(ind);
+    [kernel_contribution, train_variances_contribution, test_variances_contribution] = ...
+        wl_subtree_kernel_mex(responses, graph_ind, train_graphs, test_graphs);
 
-      feature_vectors(i, :) = histc(labels, label_set)';
-    end
-
-    % the kernel is the outer product of the feature vectors
-    kernel_matrix = kernel_matrix + ...
-        feature_vectors(train_graphs, :) * feature_vectors(test_graphs, :)';
+    kernel_matrix = kernel_matrix + kernel_contribution;
 
     if (normalize)
-      for i = 1:num_train_graphs
-        train_variances(i) = train_variances(i) + ...
-            feature_vectors(train_graphs(i), :) * feature_vectors(train_graphs(i), :)';
-      end
-      for i = 1:num_test_graphs
-        test_variances(i) = test_variances(i) + ...
-            feature_vectors(test_graphs(i), :) * feature_vectors(test_graphs(i), :)';
-      end
+      train_variances = train_variances + train_variances_contribution;
+      test_variances  = test_variances  + test_variances_contribution;
     end
-    
+
     % exit after h applications of the WL transformation
     if (iteration == h)
       break;
     end
 
     % perform the WL transformation and compression
-    responses = compress_signatures(create_signatures(data, responses));
-    
+    responses = create_and_compress_signatures(data, responses);
+
     iteration = iteration + 1;
   end
 
