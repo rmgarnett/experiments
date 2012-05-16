38ad5efd42b0048e54aca7cd29ed4d36e6ebb543
rmgarnett <romanempire@gmail.com>
2012-05-16 13:52:31 -0400

    minor changes

--- optimal_learning/search/nips/nips_experiment_search.m
+++ optimal_learning/search/nips/nips_experiment_search.m
@@ -1,20 +1,22 @@
-check_search_experiment_options;
+required_options = {'num_additional', 'seed', 'num_experiments', ...
+                    'num_evaluations', 'max_lookahead', 'report'};
+check_required_options;
 
 if (options_defined)
 
-  data_directory = '~/work/data/nips_papers/processed/top_venues/';
-  load([data_directory 'nips_graph_pca_vectors'], 'data');
-  load([data_directory 'top_venues_graph'], 'nips_index');
+  data_directory = '~/work/data/citeseer/processed/venue_subgraph/';
+  load([data_directory 'connected_venue_graph_pca_vectors'], 'data');
+  load([data_directory 'venue_subgraph'], 'connected_positive_node_ids');
 
   num_observations = size(data, 1);
 
   responses = 2 * ones(num_observations, 1);
-  responses(nips_index) = 1;
+  responses(connected_positive_node_ids) = 1;
 
   if (~exist('probability_function', 'var'))
     setup_nips_knn;
   end
-
+ 
   [results, elapsed] = perform_search_experiment(data, responses, ...
           num_additional, seed, probability_function, probability_bound, ...
           num_experiments, num_evaluations, max_lookahead, report);
