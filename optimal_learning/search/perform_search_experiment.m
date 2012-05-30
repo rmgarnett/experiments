c42a0f1de6d8873e2d36a900c5bc68f32377d5dd
rmgarnett <romanempire@gmail.com>
2012-05-30 14:47:12 -0400

    yanking out nonsense

--- optimal_learning/search/perform_search_experiment.m
+++ optimal_learning/search/perform_search_experiment.m
@@ -1,53 +1,29 @@
-function [results, elapsed] = perform_search_experiment(data, ...
-          responses, num_additional, seed, probability_function, ...
-          probability_bound, num_experiments, num_evaluations, ...
-          max_lookahead, report)
+function [utilities, elapsed] = perform_search_experiment(data, ...
+          responses, seed, probability_function, probability_bound, ...
+          num_experiments, num_evaluations, lookahead, report)
 
   stream = RandStream('mt19937ar', 'seed', seed);
   RandStream.setGlobalStream(stream);
 
-  results = zeros(num_experiments, num_evaluations, max_lookahead);
-  elapsed = zeros(num_experiments, max_lookahead);
+  % always add one positive point
+  r = randperm(nnz(responses == 1));
+  train_ind = logical_ind(responses == 1, r(1));
 
-  for experiment = 1:num_experiments
+  start = tic;
 
-    % always add one positive point
-    r = randperm(nnz(responses == 1));
-    train_ind = logical_ind(responses == 1, r(1:(num_additional + 1)));
+  [~, utilities] = active_search(data, responses, train_ind, ...
+          probability_function, probability_bound, lookahead, ...
+          num_evaluations, false);
+  utilities = utilities -  1;
+  
+  elapsed = toc(start);
 
-    r = randperm(nnz(responses ~=1));
-    train_ind = [train_ind; logical_ind(responses ~= 1, r(1:(num_additional + 1)))];
-
-    for lookahead = 1:max_lookahead
-      start = tic;
-      [~, utilities] = active_search(data, responses, train_ind, ...
-              probability_function, probability_bound, lookahead, ...
-              num_evaluations, true);
-      utilities = utilities - num_additional;
-      results(experiment, :, lookahead) = utilities;
-      elapsed(experiment, lookahead) = toc(start);
-
-      for i = 1:lookahead
-        fprintf('experiment %i: %i-step utility: %i, mean: %.2f, took: %.2fs, mean: %.2fs', ...
-                experiment, ...
-                i, ...
-                results(experiment, end, i), ...
-                mean(results(1:experiment, end, i)), ...
-                elapsed(experiment, i), ...
-                mean(elapsed(1:experiment, i)));
-        fprintf('\n');
-      end
-    end
-
-    for steps = 1:numel(report)
-      for lookahead = 1:max_lookahead
-        fprintf('%i-step-utility after %i steps: %i, mean: %.2f', ...
-                lookahead, ...
-                report(steps), ...
-                results(experiment, report(steps), lookahead), ...
-                mean(results(1:experiment, report(steps), lookahead)));
-        fprintf('\n');
-      end
-    end
+  for steps = 1:numel(report)
+    fprintf('%i-step utility after %i steps: %i\n', ...
+            lookahead, ...
+            report(steps), ...
+            utilities(report(steps)));
   end
 end
+
+
