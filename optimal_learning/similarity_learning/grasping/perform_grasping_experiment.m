3eda67ee4b13852d7a273dbbf25972749f19b498
rmgarnett <romanempire@gmail.com>
2012-05-30 16:48:02 -0400

    no longer needed

--- optimal_learning/similarity_learning/grasping/perform_grasping_experiment.m
+++ optimal_learning/similarity_learning/grasping/perform_grasping_experiment.m
@@ -1,15 +1,16 @@
 prepare_grasping_data;
 
-h              = 2;
-train_fraction = 20.05;
-num_samples    = 100;
-lookahead      = 1;
-verbose        = true;
-k_fraction     = 5 / num_graphs;
-
-tolerance      = 1e-1;
-max_iterations = 10;
+h                = 2;
+num_samples      = 10;
+lookahead        = 1;
+verbose          = false;
+k                = 3;
+num_evaluations  = 10;
+train_fraction   = 1;
+num_iterations   = 5;
+use_map          = false;
+use_max          = true;
+train_graphs     = [2 6 7 15 23 27 31];
 
 setup_lp;
-
 perform_online_similarity_experiment;
